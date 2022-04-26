32a4996d10a7a745da95d263a01c74c644cf2b96
yongman Lee <dydaks7878@gmail.com>
2022-04-26 21:01:47 +0900

    프로젝트 시작

---
+++ src/test/java/com/greenlanton/greenlanton/GreenLantonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greenlanton.greenlanton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenLantonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
